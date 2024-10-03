d796efa3fd23191d7044a3bcd12cb451d6a6b04a
Patrick Vogt <patrick.vogt@unibas.ch>
2024-10-04 00:44:55 +0200

    res

--- pkg/thing/thing.go
+++ pkg/thing/thing.go
@@ -39,7 +39,7 @@ func (t *Thing) IsDead() bool {
 	d := t.LP < 0
 	if d && t.LP != -255{
 		t.LP = -255
-		if suf, err := img.Load("res/ball.png"); err == nil {
+		if suf, err := img.Load("res/meat_dead.png"); err == nil {
 			suf.SetAlphaMod(200)
 			t.surface = suf
 		} else {
