fccd9cf8e6ea0ab9af1222b44634c178fe0043c1
Patrick Vogt <patrick.vogt@unibas.ch>
2024-10-08 13:45:18 +0200

    display player gold

--- pkg/loop/menu_loop/menu_loop.go
+++ pkg/loop/menu_loop/menu_loop.go
@@ -1,11 +1,13 @@
 package menuloop
 
 import (
+	"fmt"
 	"log/slog"
 
 	"github.com/veandco/go-sdl2/sdl"
 	"github.com/vogtp/eggsis-go/pkg/controlls"
 	"github.com/vogtp/eggsis-go/pkg/controlls/position"
+	"github.com/vogtp/eggsis-go/pkg/fontmanager"
 	"github.com/vogtp/eggsis-go/pkg/player"
 	vertor "github.com/vogtp/eggsis-go/pkg/vector"
 )
@@ -30,8 +32,8 @@ func Run(window *sdl.Window) bool {
 	defer func() { buttons = buttons[:0] }()
 	buttons = append(buttons, controlls.NewButton("Quit", position.BottmLeft(windowSurface.W, windowSurface.H, 250, 100), func() { stop = true }))
 	buttons = append(buttons, controlls.NewButton("Start Fight", position.BottmRight(windowSurface.W, windowSurface.H, 250, 100), func() { running = false }))
-	
-	choose:=modsMenu()
+
+	choose := modsMenu()
 	defer choose()
 	if player.Instance == nil {
 		slog.Info("Player Menu", "player", player.Instance)
@@ -40,7 +42,16 @@ func Run(window *sdl.Window) bool {
 	}
 	for running && !stop {
 		windowSurface.FillRect(nil, 0)
+		font := fontmanager.GetFont(24)
+		text, err := font.RenderUTF8Blended(fmt.Sprintf("Gold: %v", player.Instance.Gold), sdl.Color{R: 0, G: 0, B: 255, A: 255})
+		if err != nil {
+			panic(err)
+		}
 
+		// Draw the text around the center of the window
+		if err = text.Blit(nil, windowSurface, &sdl.Rect{X: 0, Y: 0, W: 0, H: 0}); err != nil {
+			panic(err)
+		}
 		for _, b := range buttons {
 			b.Paint(windowSurface)
 		}
